85f90bdf01ee24019e7c104cc51adc86001c5443
Thays <thayscasado@hotmail.com>
2021-04-19 19:23:28 -0700

    Using variables

---
+++ Learning PHP/1. The Basics/variables.php
@@ -0,0 +1,21 @@
+<?php
+
+  // Variables
+  $age = 34; // always start with a dollar sign
+
+  echo $age;
+
+  $double_age = $age * 2;
+
+  echo $double_age;
+
+  // Constants
+  define('VERSION', 1.1);
+  echo VERSION;
+
+
+
+  // STRING
+  $name = "Joe";
+  $first_name = 'J';
+?>
